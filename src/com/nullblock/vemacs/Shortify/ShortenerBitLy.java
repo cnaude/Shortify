462ba1422412c4b5c9d8e1f0045a096550471a78
vemacs <gentoo@outlook.com>
2012-11-11 07:36:00 -0700

    Using pure JSON for bit.ly request

--- src/com/nullblock/vemacs/Shortify/ShortenerBitLy.java
+++ src/com/nullblock/vemacs/Shortify/ShortenerBitLy.java
@@ -16,25 +16,44 @@ public class ShortenerBitLy implements Shortener {
 	}
 	
 	public String getShortenedUrl(String toshort) throws ShortifyException {
-	    URL shorted = null;
-		try {
-			shorted = new URL("http://api.bit.ly/v3/shorten?login=" + u + "&apiKey=" + a + "&longUrl=" + toshort + "&format=txt");
-		} catch (MalformedURLException e1) {
+	    String shortUrl = "";
+	    try
+	    {
+	    	String bitlyUrl = "http://api.bit.ly/v3/shorten?login=" + u + "&apiKey=" + a + "&longUrl=" + toshort "&format=json";
+	        URLConnection conn = new URL(bitlyUrl).openConnection();
+	        conn.setDoOutput(true);
+	        conn.setRequestProperty("Content-Type", "application/json");
+	        OutputStreamWriter wr = 
+	                     new OutputStreamWriter(conn.getOutputStream());
+	        wr.write("{\"longUrl\":\"" + toshort + "\"}");
+	        wr.flush();
 
-		}
-        String inputLine = null;
-        try {
-        BufferedReader in = new BufferedReader(
-        new InputStreamReader(shorted.openStream()));
-        while ((inputLine = in.readLine()) != null)
-    		return inputLine;
-        in.close();
-        }
+	        BufferedReader rd = 
+	                     new BufferedReader(
+	                     new InputStreamReader(conn.getInputStream()));
+	        String line;
+
+	        while ((line = rd.readLine()) != null)
+	        {
+	            if (line.indexOf("hash") > -1)
+	            {
+	                shortUrl = "http://bit.ly/" + line.substring(13, line.length() - 2);
+	                break;
+	            }
+	        }
+
+	        wr.close();
+	        rd.close();
+	    }
+	    catch (MalformedURLException ex)
+	    {
+	    }
 	    catch (IOException ex)
 	    {    
 	    	throw new ShortifyException("Unable to shorten: "+ex.getMessage());
 	    }
-		return inputLine;
+
+	    return shortUrl;
 	}
 
 }
