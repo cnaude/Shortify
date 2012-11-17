f381ff9f61933b461233437184d92fd9c59f0581
vemacs <gentoo@outlook.com>
2012-11-17 11:35:24 -0700

    Tells user about config errors

--- src/com/nullblock/vemacs/Shortify/Shortify.java
+++ src/com/nullblock/vemacs/Shortify/Shortify.java
@@ -1,6 +1,7 @@
 package com.nullblock.vemacs.Shortify;
 
 
+import org.bukkit.ChatColor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -18,10 +19,18 @@ public final class Shortify extends JavaPlugin {
         config.addDefault("auto-update", "true");
         config.options().copyDefaults(true);
         saveConfig();
+        if(this.getConfig().getString("shortener").equals("bitly")){
+            if(this.getConfig().getString("bitlyAPI").equals("none") && this.getConfig().getString("bitlyAPI").equals("none")){
+            	getLogger().info(ChatColor.RED + "bitly is not properly configured, see config.yml for details");
+        }
+            if(this.getConfig().getString("shortener").equals("googl") && this.getConfig().getString("googAPI").equals("none")){
+                	getLogger().info(ChatColor.RED + "googl is not properly configured, see config.yml for details");
+            }
         if(this.getConfig().getString("auto-update").equals("true")){
         	Updater updater = new Updater(this, "Shortify", this.getFile(), Updater.UpdateType.DEFAULT, false);
         	getLogger().info("The latest reviewed version of Shortify is " + updater.getLatestVersionString());
         	getLogger().info("You are running version" + this.getDescription().getVersion());
+        }
         }
     }
     
