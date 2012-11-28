77c0b2dd6390f06584ca793d42cf6a4d801cf730
vemacs <gentoo@outlook.com>
2012-11-28 08:52:32 -0700

    Referencing URLReader

--- src/com/nullblock/vemacs/Shortify/ShortenerIsGd.java
+++ src/com/nullblock/vemacs/Shortify/ShortenerIsGd.java
@@ -1,39 +1,14 @@
 package com.nullblock.vemacs.Shortify;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
+import java.io.*;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class ShortenerIsGd implements Shortener {
-
 	public String getShortenedUrl(String toshort) throws ShortifyException {
-		URL shorted = null;
-		try {
-			shorted = new URL("http://is.gd/create.php?format=simple&url="
-					+ toshort);
-		} catch (MalformedURLException e1) {
-
-		}
-		String inputLine = null;
-		try {
-			BufferedReader in = new BufferedReader(new InputStreamReader(
-					shorted.openStream()));
-			while ((inputLine = in.readLine()) != null)
-				return inputLine;
-			in.close();
-		} catch (IOException ex) {
-			try {
-				String decoded = java.net.URLDecoder.decode(toshort, "UTF-8");
-				throw new ShortifyException("Unable to shorten via is.gd: "
-						+ decoded);
-			} catch (UnsupportedEncodingException e) {
-			}
-
-		}
-		return inputLine;
+		String toread = "http://is.gd/create.php?format=simple&url=" + toshort;
+		String shortened = URLReader.getShortenedUrl(toread, "is.gd");
+		return shortened;
 	}
-
 }
