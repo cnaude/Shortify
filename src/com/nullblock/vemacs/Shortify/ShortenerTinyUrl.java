1043edf4c7269e388eef7dc036c6380c605646d9
vemacs <gentoo@outlook.com>
2012-11-28 08:51:19 -0700

    Switched to URLReader Implementation for TinyURL

--- src/com/nullblock/vemacs/Shortify/ShortenerTinyUrl.java
+++ src/com/nullblock/vemacs/Shortify/ShortenerTinyUrl.java
@@ -7,24 +7,8 @@ import java.net.URL;
 
 public class ShortenerTinyUrl implements Shortener {
 	public String getShortenedUrl(String toshort) throws ShortifyException {
-		URL shorted = null;
-		try {
-			shorted = new URL("http://tinyurl.com/api-create.php?url="
-					+ toshort);
-		} catch (MalformedURLException e1) {
-
-		}
-		String inputLine = null;
-		try {
-			BufferedReader in = new BufferedReader(new InputStreamReader(
-					shorted.openStream()));
-			while ((inputLine = in.readLine()) != null)
-				return inputLine;
-			in.close();
-		} catch (IOException ex) {
-			throw new ShortifyException("Unable to shorten via tinyurl.com: "
-					+ ex.getMessage());
-		}
-		return inputLine;
+		String toread = "http://tinyurl.com/api-create.php?url=" + toshort;
+		String shortened = URLReader.getShortenedUrl(toread, "TinyURL");
+		return shortened;
 	}
 }
