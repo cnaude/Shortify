2b613670dc7886149d75cd559a52e1c105f1e01a
vemacs <gentoo@outlook.com>
2012-12-02 10:21:35 -0700

    Implemented classic

--- src/com/nullblock/vemacs/Shortify/ShortifyClassicListener.java
+++ src/com/nullblock/vemacs/Shortify/ShortifyClassicListener.java
@@ -74,26 +74,27 @@ public class ShortifyClassicListener implements Listener {
 	@EventHandler(priority = EventPriority.LOW)
 	public void playerChat(AsyncPlayerChatEvent e) {
 		String message = e.getMessage();
-		// regex operations aren't that expensive, now www. to www999. URLs
+		// regex operations aren't that expensive, now www. to www9s99. URLs
 		// should be shortened
 		if (e.getPlayer().hasPermission("shortify.shorten")) {
 			// REGEX from Daring Fireball
 			Pattern p = Pattern
-					.compile("(?i)\\b((?:https?://|www\\d{0,3}[.]|[a-z0-9.\\-]+[.][a-z]{2,4}/)(?:[^\\s()<>]+|\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\))+(?:\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\)|[^\\s`!()\\[\\]{};:'\".,<>?������]))");
+					.compile("(?i)\\b((?:https?://|www\\d{0,3}[.]|[a-z0-9.\\-]+[.][a-z]{2,4}/)(?:[^\\s()<>]+|\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\))+(?:\\(([^\\s()<>]+|(\\([^\\s()<>]+\\)))*\\)|[^\\s`!()\\[\\]{};:'\".,<>?ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½]))");
 			Matcher m = p.matcher(message);
 			StringBuffer sb = new StringBuffer();
 			String urlTmp = "";
 			String minlength = plugin.getConfig().getString("minlength");
 			int min = Integer.parseInt(minlength);
+			String output = "The following URLs were shortened: ";
 			while (m.find()) {
 				try {
 					urlTmp = m.group(1);
 					if (urlTmp.length() > min) {
 						try {
-							urlTmp = shortener
+							output = output + ChatColor.RESET + ChatColor.AQUA + shortener
 									.getShortenedUrl(java.net.URLEncoder
-											.encode(urlTmp, "UTF-8"));
-							// might as well put the encoder in the listener1 to
+											.encode(urlTmp, "UTF-8")) + " ,";
+							// might as well put the encoder in the listener to
 							// prevent possible injections
 						} catch (UnsupportedEncodingException e1) {
 							// do absolutely nothing
@@ -103,9 +104,11 @@ public class ShortifyClassicListener implements Listener {
 					Bukkit.getConsoleSender().sendMessage(
 							ChatColor.RED + "Warning: " + e1.getMessage());
 				}
+				m.appendReplacement(sb, "");
 				sb.append(urlTmp);
 			}
-			e.setMessage(sb.toString());
+			m.appendTail(sb);
+			e.setMessage(sb.substring(0, sb.length() - 3).toString());
 		}
 	}
 }
