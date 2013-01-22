604704890496b79359560ab2ee66cd03bd7b27a7
vemacs <gentoo@outlook.com>
2013-01-22 15:53:31 -0700

    added texttocolor

--- src/main/java/com/nullblock/vemacs/Shortify/util/ShortifyUtility.java
+++ src/main/java/com/nullblock/vemacs/Shortify/util/ShortifyUtility.java
@@ -44,4 +44,44 @@ public class ShortifyUtility {
 					+ ex.getMessage());
 		}
 	}
+	
+	public static String textToColor(String text)
+	{
+		//from http://dev.bukkit.org/server-mods/chatman/pages/code/text-to-color/
+		//there are probably better implementations available, this is missing underlines, bold, etc...
+		//look into encoding
+		text = text.replaceAll("&0", ChatColor.BLACK+"");
+		text = text.replaceAll("&1", ChatColor.DARK_BLUE+"");
+		text = text.replaceAll("&2", ChatColor.DARK_GREEN+"");
+		text = text.replaceAll("&3", ChatColor.DARK_AQUA+"");
+		text = text.replaceAll("&4", ChatColor.DARK_RED+"");
+		text = text.replaceAll("&5", ChatColor.DARK_PURPLE+"");
+		text = text.replaceAll("&6", ChatColor.GOLD+"");te
+		text = text.replaceAll("&7", ChatColor.GRAY+"");
+		text = text.replaceAll("&8", ChatColor.DARK_GRAY+"");
+		text = text.replaceAll("&9", ChatColor.BLUE+"");
+		text = text.replaceAll("&A", ChatColor.GREEN+"");
+		text = text.replaceAll("&B", ChatColor.AQUA+"");
+		text = text.replaceAll("&C", ChatColor.RED+"");
+		text = text.replaceAll("&D", ChatColor.LIGHT_PURPLE+"");
+		text = text.replaceAll("&E", ChatColor.YELLOW+"");
+		text = text.replaceAll("&F", ChatColor.WHITE+"");
+		text = text.replaceAll("&a", ChatColor.GREEN+"");
+		text = text.replaceAll("&b", ChatColor.AQUA+"");
+		text = text.replaceAll("&c", ChatColor.RED+"");
+		text = text.replaceAll("&d", ChatColor.LIGHT_PURPLE+"");
+		text = text.replaceAll("&e", ChatColor.YELLOW+"");
+		text = text.replaceAll("&u", ChatColor.UNDERLINE+"");
+		text = text.replaceAll("&U", ChatColor.UNDERLINE+"");
+		text = text.replaceAll("&n", ChatColor.BOLD+"");
+		text = text.replaceAll("&N", ChatColor.BOLD+"");
+		text = text.replaceAll("&o", ChatColor.ITALIC+"");
+		text = text.replaceAll("&O", ChatColor.ITALIC+"");
+		text = text.replaceAll("&i", ChatColor.ITALIC+"");
+		text = text.replaceAll("&I", ChatColor.ITALIC+"");
+		text = text.replaceAll("&k", ChatColor.MAGIC+"");
+		text = text.replaceAll("&K", ChatColor.MAGIC+"");
+		text = text.replaceAll("&r", ChatColor.RESET+"");
+		return text;
+	}
 }
